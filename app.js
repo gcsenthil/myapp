ac67bf9433aeb541e57d04e70ce3043bc31f4c8a
gcsenthil <g.c.senthil@gmail.com>
2022-10-11 17:01:28 +0530

    update sitemapurl

--- app.js
+++ app.js
@@ -45,18 +45,19 @@ app.get('/sitemap.xml', function(req, res) {
     const pipeline = smStream.pipe(createGzip())
 
     // pipe your entries or directly write them.
-    
+    smStream.write({ url: 'https://www.neherald.com/',  changefreq: 'daily', priority: 1 })
     for(i=0;i<newsdata.news.all.length;i++){  
     smStream.write({ url: '/news?id='+newsdata.news.all[i].id,  changefreq: 'daily', priority: 0.3 })
     }
 
     smStream.write({ url: '/aboutus-tripur',  changefreq: 'monthly',  priority: 0.7 })
     smStream.write({ url: '/photos',  changefreq: 'monthly',  priority: 0.7 })
+    smStream.write({ url: '/photos',  changefreq: 'monthly',  priority: 0.7 })
     smStream.write({ url: '/contactus',  changefreq: 'monthly',  priority: 0.7 })
     smStream.write({ url: '/governer',  changefreq: 'monthly',  priority: 0.7 })
     smStream.write({ url: '/chiefminister',  changefreq: 'monthly',  priority: 0.7 })
     smStream.write({ url: '/ministers',  changefreq: 'monthly',  priority: 0.7 })
-    //smStream.write({ url: '/page-3/'})    // changefreq: 'weekly',  priority: 0.5
+    smStream.write({ url: '/dailynewsphoto/', changefreq: 'weekly',  priority: 0.5})
    // smStream.write({ url: '/page-4/',   img: "http://www.neherald.com" })
     /* or use
     Readable.from([{url: '/page-1'}...]).pipe(smStream)
